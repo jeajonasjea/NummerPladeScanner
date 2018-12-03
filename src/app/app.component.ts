c9c4a85d5950ee84513077afc1d0cbd6be0d56ef
Casper Ludvigsen <casp6239@edu.eal.dk>
2018-12-03 14:42:45 +0100

    startet project and added angular pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NummerpladeScanner';
+  installable: boolean = true;
+  
+  constructor(){
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+     // Prevent Chrome 67 and earlier from automatically showing the prompt
+     e.preventDefault();
+     // Stash the event so it can be triggered later.
+     deferredPrompt = e;
+     this.installable = true;
+   });
+  }
 }
